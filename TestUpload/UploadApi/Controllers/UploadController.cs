08145164973a371d23c090593313a42fce51f656
Vsevolod Goloviznin <vsevolod@neovadalabs.com>
2014-04-03 22:06:15 +0400

    Initial upload

---
+++ TestUpload/UploadApi/Controllers/UploadController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace UploadApi.Controllers
+{
+    public class UploadController : ApiController
+    {
+        public async Task<HttpResponseMessage> Post()
+        {
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            const string fileDirectory = "UploadedFiles/";
+            string root = HttpContext.Current.Server.MapPath("~/" + fileDirectory);
+            var provider = new MultipartFormDataStreamProvider(root);
+
+            try
+            {
+                await Request.Content.ReadAsMultipartAsync(provider);
+                
+                var filepath = "";
+                foreach (var file in provider.FileData)
+                {
+                    var filename = Guid.NewGuid() + file.Headers.ContentDisposition.FileName.Replace("\"", "");
+                    var fileInfo = new FileInfo(file.LocalFileName);
+
+                    File.Move(fileInfo.FullName, root + filename);
+                }
+
+                return new HttpResponseMessage
+                {
+                    Content = new StringContent(filepath)
+                };
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+    }
+}
